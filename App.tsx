bfec7eb37a7260391cefb8cea7a565c0ae0eff57
Miguel Caballero <mcabs3@gmail.com>
2018-07-25 10:11:20 -0500

    Added nav reference for global use

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@
  * @flow
  */
 import './ReactotronConfig';
+import NavigatorService from './src/services/navigation';
 import React, { Component } from 'react';
 import {
   Platform,
@@ -16,16 +17,25 @@ import { Provider } from 'react-redux';
 import configureStore from './src/store/configureStore';
 import AppRouter from './src/views/router';
 
-interface Props { };
-interface State { };
+interface Props { }
+interface State { }
+
+// Allow access to navigation in sagas
+export let navigatorRef: any;
 
 const { store, persistor } = configureStore();
 export default class App extends Component<Props, State> {
+  navigatorRef: any;
+
+  componentDidMount() {
+    navigatorRef = this.navigatorRef;
+  }
+
   render() {
     return (
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <AppRouter />
+          <AppRouter ref={(nav: any) => { NavigatorService.setContainer(nav); }} />
         </PersistGate>
       </Provider>
 
