bf4df31e9d83597403b37ddce59c53a1b938be02
Miguel Caballero <mcabs3@gmail.com>
2018-07-26 11:50:19 -0500

    removed not needed dependencies

--- App.tsx
+++ App.tsx
@@ -7,24 +7,18 @@ import './ReactotronConfig';
 import NavigatorService from './src/services/navigation';
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+  Alert
 } from 'react-native';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import configureStore from './src/store/configureStore';
 import AppRouter from './src/views/router';
 
-interface Props { }
-interface State { }
-
 // Allow access to navigation in sagas
 export let navigatorRef: any;
 
 const { store, persistor } = configureStore();
-export default class App extends Component<Props, State> {
+export default class App extends Component {
   navigatorRef: any;
 
   componentDidMount() {
@@ -42,4 +36,3 @@ export default class App extends Component<Props, State> {
     );
   }
 }
-
