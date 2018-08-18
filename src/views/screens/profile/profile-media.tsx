533119be8cdafe89bfa0a29028f7f741fe7b3e2e
Miguel Caballero <mcabs3@gmail.com>
2018-08-18 09:37:33 -0500

    Fixed keyboard issue in Profile Media

--- src/views/screens/profile/profile-media.tsx
+++ src/views/screens/profile/profile-media.tsx
@@ -3,11 +3,12 @@ import Screen from '../../components/screen';
 import ShadowBox from '../../components/theme/shadow-box';
 import { MediaGrid } from '../../components/theme/media-grid';
 import { TextArea, PrimaryButton } from '../../components/theme';
-import { View,TouchableOpacity,Text, Alert } from 'react-native';
+import { View, TouchableOpacity, Text, Alert, KeyboardAvoidingView } from 'react-native';
 import theme, { Device } from '../../../assets/styles/theme';
 import WonderAppState from '../../../types/wonder-app-state';
 import User from '../../../types/user';
 import { connect } from 'react-redux';
+import { KeyboardDismissView } from '../../components/keyboard-dismiss-view';
 
 const mapState = (state: WonderAppState) => ({
   currentUser: state.user.profile
@@ -19,32 +20,41 @@ interface Props {
 
 class ProfileMediaScreen extends React.Component<Props> {
 
- 
+
 
   render() {
-    const { navigation,currentUser } = this.props;
+    const { navigation, currentUser } = this.props;
     return (
       <Screen horizontalPadding={20}>
-        <View style={{ flex: 1, justifyContent: 'center' }}>
-          <ShadowBox>
-            <MediaGrid
-              width={Device.WIDTH - 80}
-              gutter={2}
-              onNewPicture={() => navigation.navigate('ProfileCamera')}
-              onNewVideo={() => navigation.navigate('ProfileVideo')}              
-            />
-            <TextArea
-              label="About Me"
-              placeholder="Take this time to describe yourself, life experience, hobbies, and anything else that makes you wonderful..."
-              maxLength={200}
-            />
-            <View style={{ marginTop: 10 }}>
-              <PrimaryButton title="DONE" onPress={() => navigation.goBack()} />
-            </View>
-          
-          </ShadowBox>
-        </View>
-      </Screen>
+        <KeyboardAvoidingView
+          behavior="position"
+          style={{ flex: 1 }}
+          contentContainerStyle={{ flex: 1 }}
+        >
+
+          <View style={{ flex: 1, justifyContent: 'center' }}>
+            <ShadowBox>
+              <KeyboardDismissView>
+                <MediaGrid
+                  width={Device.WIDTH - 80}
+                  gutter={2}
+                  onNewPicture={() => navigation.navigate('ProfileCamera')}
+                  onNewVideo={() => navigation.navigate('ProfileVideo')}
+                />
+                <TextArea
+                  label="About Me"
+                  placeholder="Take this time to describe yourself, life experience, hobbies, and anything else that makes you wonderful..."
+                  maxLength={200}
+                />
+                <View style={{ marginTop: 10 }}>
+                  <PrimaryButton title="DONE" onPress={() => navigation.goBack()} />
+                </View>
+              </KeyboardDismissView>
+            </ShadowBox>
+          </View>
+
+        </KeyboardAvoidingView>
+      </Screen >
     );
   }
 }
