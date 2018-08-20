de8f84108f21124f04683157f49226058586eea2
Miguel Caballero <mcabs3@gmail.com>
2018-08-20 13:21:40 -0500

    started push integration

--- android/app/src/main/java/com/thewonderapp/MainApplication.java
+++ android/app/src/main/java/com/thewonderapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new ReactVideoPackage(),
             new RNCameraPackage(),
             new LinearGradientPackage(),
