01e9f077af684ccc764ca533637f1255afc5690f
webdatavisualdev <cli698638@gmail.com>
2018-08-24 00:38:45 -0700

    Implemented FCM push notification

--- android/app/src/main/java/com/thewonderapp/MainApplication.java
+++ android/app/src/main/java/com/thewonderapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactApplication;
+import com.evollu.react.fcm.FIRMessagingPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import org.reactnative.camera.RNCameraPackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FIRMessagingPackage(),
             new ReactNativePushNotificationPackage(),
             new ReactVideoPackage(),
             new RNCameraPackage(),
