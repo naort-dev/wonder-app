e760bbfed476430a3dc1ce858d8e34984d5c2a64
Miguel Caballero <mcabs3@gmail.com>
2018-09-15 17:31:50 -0500

    Added initializeSdk call to MainApplication

--- android/app/src/main/java/com/thewonderapp/MainApplication.java
+++ android/app/src/main/java/com/thewonderapp/MainApplication.java
@@ -72,5 +72,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    FacebookSdk.sdkInitialize(getApplicationContext());
   }
 }
