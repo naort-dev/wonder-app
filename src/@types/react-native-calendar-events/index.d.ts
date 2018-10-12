ca948a8c120550230c85015591a5e4c8d7b1ab48
Miguel Caballero <mcabs3@gmail.com>
2018-10-12 11:17:33 -0500

    Updated types for untyped libs

--- src/@types/react-native-calendar-events/index.d.ts
+++ src/@types/react-native-calendar-events/index.d.ts
@@ -3,9 +3,12 @@ declare module 'react-native-calendar-events' {
   export type RNCalendarEventOccurrence = 'daily' | 'weekly' | 'monthly' | 'yearly';
 
   export default class RNCalendarEvents {
+    static authorizationStatus(): Promise<RNCalendarEventPermissionLevel>;
     static authorizeEventStore(): Promise<RNCalendarEventPermissionLevel>;
     static fetchAllEvents(startDate: Date, endDate: Date, calendars?: RNCalendarCalendar[]): Promise<any>;
     static findCalendars(): Promise<RNCalendarCalendar[]>;
+    static saveEvent(title: string, details: Partial<RNCalendarEvent>, options: any): Promise<string>;
+    static findEventById(id: string): Promise<RNCalendarEvent | null>;
   }
 
   export interface RNCalendarEvent {
@@ -15,11 +18,20 @@ declare module 'react-native-calendar-events' {
     startDate: Date;
     endDate: Date;
     allDay: boolean;
-    url: string; // iOS only
     location: string;
+    url: string; // iOS only
     description: string; // Android only
     recurrence?: RNCalendarEventOccurrence;
   }
 
-  export interface RNCalendarCalendar { }
+  export interface RNCalendarCalendar {
+    id: string;
+    title: string;
+    type: string;
+    source: string;
+    isPrimary: boolean;
+    allowsModifications: boolean;
+    color: string;
+    allowedAvailabilities: any[];
+  }
 }
