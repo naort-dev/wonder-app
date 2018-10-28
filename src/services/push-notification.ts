5336b3c253bd53e92f0d1cd42b2375376bc67a43
hnka <mchma@cin.ufpe.br>
2018-10-28 17:58:58 -0300

    adding sender id to push notification service

--- src/services/push-notification.ts
+++ src/services/push-notification.ts
@@ -1,8 +1,8 @@
 import RNPushNotification, {
   PushNotification,
   PushNotificationObject
-} from "react-native-push-notification";
-import { Alert } from "react-native";
+} from 'react-native-push-notification';
+import { Alert } from 'react-native';
 
 interface RNPushNotificationToken {
   os: string;
@@ -15,26 +15,27 @@ class PushNotificationService {
   public onRegister?: onRegisterToken;
   public onNotification?: (notification: PushNotificationObject) => void;
   private lastId: number = 0;
+  private senderID: string = '487922911515';
 
   private handleRegister = (token: RNPushNotificationToken) => {
     this.token = token;
     if (this.onRegister) {
       this.onRegister(token);
     }
-  }
+  };
 
   private handleNotificationReceived = (notification: PushNotification) => {
     Alert.alert(
-      "A wonder-ful message for you",
+      'A wonder-ful message for you',
       notification.message.toString()
     );
-  }
+  };
 
-  configure(gcm?: string) {
+  configure() {
     RNPushNotification.configure({
       onRegister: this.handleRegister,
       onNotification: this.handleNotificationReceived,
-      senderID: gcm,
+      senderID: this.senderID,
       popInitialNotification: true,
       requestPermissions: true
     });
@@ -42,7 +43,7 @@ class PushNotificationService {
 
   localNotification = (content: PushNotificationObject): void => {
     RNPushNotification.presentLocalNotification(content);
-  }
+  };
 }
 
 export default new PushNotificationService();
