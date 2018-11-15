20b3c883d59c48e6083cf792081efe0b64ffb990
Johann Schuster <jschuster.dev@gmail.com>
2018-11-15 12:28:43 -0800

    installed different version of fast image

--- src/views/components/modals/activity-details-modal.tsx
+++ src/views/components/modals/activity-details-modal.tsx
@@ -35,7 +35,8 @@ class ActivityDetailsModal extends React.Component<ActivityDetailsModalProps> {
         rating,
         review_count,
         price_level,
-        images
+        images,
+        phone
       } = details;
 
       return (
