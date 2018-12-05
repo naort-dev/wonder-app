34e0b2d520b2f1cecdc1225ec5aa33d6c816aea1
naort-dev <naor.tzairi.upwork@gmail.com>
2018-12-05 11:35:56 -0800

    Updated bugsnag key to Integritee

--- src/utils/appConfig.ts
+++ src/utils/appConfig.ts
@@ -2,7 +2,7 @@ export const DEV: boolean = process.env.NODE_ENV === 'development';
 
 export const PURGE = DEV ? false : false;
 
-export const BUGSNAG_TOKEN: string = '38c3267508acb58c6e39fab639bfad76';
+export const BUGSNAG_TOKEN: string = 'b11b0680cca2e6736fcd9a0ba94ae172';
 export const INITIAL_HOME_SCREEN: string = DEV ? 'Home' : 'Home';
 export const INITIAL_PROFILE_SCREEN: string = DEV
   ? 'ProfileView'
